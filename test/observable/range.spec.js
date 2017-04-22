ab20fd9fe8ec6e9be81900e2b8e8b1c1483e4599
wemlion <angusfu1126@qq.com>
2017-04-23 02:01:22 +0800

    update test case

--- test/observable/range.spec.js
+++ test/observable/range.spec.js
@@ -2,7 +2,7 @@ import Observable from '../../lib/observable';
 import '../../lib/observable/range';
 
 describe('Observable.range', () => {
-  it('closed at once if zero arguments provided', () => {
+  it('closed at once if no arguments provided', () => {
     const callback = jest.fn();
     const subscription = Observable.range().subscribe(callback);
     expect(callback).not.toBeCalled();
@@ -12,8 +12,12 @@ describe('Observable.range', () => {
   it('won\'t emit anything if second arguments not provided', () => {
     const callback = jest.fn();
     const subscription = Observable.range(5).subscribe(callback);
+    const callback2 = jest.fn();
+    const subscription2 = Observable.range(null, 5).subscribe(callback);
     expect(callback).not.toBeCalled();
     expect(subscription.closed).toBe(true);
+    expect(callback2).not.toBeCalled();
+    expect(subscription2.closed).toBe(true);
   });
 
   it('emits a series of number from 2 to 10', () => {
